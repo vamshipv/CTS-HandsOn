e584c4aac64ee0f948b0f519010b046431a8a4a3
Vamshi PV <50285435+vamshipv@users.noreply.github.com>
2021-08-31 13:49:05 +0530

    Code Analysis

---
+++ Code_Analysis/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Analysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1
+            //string[] names = { "vamshi", "ravindra", "sanath", "poorna" };
+
+            //var namesOfV = names.Where(n => n.StartsWith("V"));
+
+
+            //Task 2
+            //Console.WriteLine("Enter the N value");
+            //int n = int.Parse(Console.ReadLine());
+
+            //int i = 0;
+            //while(i<n)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+            try
+            {
+                int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+                int c = a / b;
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
